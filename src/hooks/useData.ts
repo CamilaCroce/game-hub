65a23c21b473c333347510ceb88620e9b598ab34
Camila Croce <camcroce.cc@gmail.com>
2024-06-01 20:36:07 +0200

    Filter games by genre

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,37 +1,45 @@
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
+import { AxiosRequestConfig, CanceledError } from "axios";
 
 interface FetchResponse<T> {
   const: number;
   results: T[];
 }
 
-const useData = <T>(endpoint: string) => {
+const useData = <T>(
+  endpoint: string,
+  requestConfig?: AxiosRequestConfig,
+  deps?: any[]
+) => {
   const [data, setData] = useState<T[]>([]);
   const [error, setError] = useState([]);
   const [isLoading, setLoading] = useState(false);
 
-  useEffect(() => {
-    const controller = new AbortController();
+  useEffect(
+    () => {
+      const controller = new AbortController();
+      const config = { ...requestConfig, signal: controller.signal };
 
-    setLoading(true);
-    apiClient
-      .get<FetchResponse<T>>(endpoint, { signal: controller.signal })
-      .then((res) => {
-        setData(res.data.results);
-        setLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) {
-          return;
-        } else {
-          setError(err.message);
+      setLoading(true);
+      apiClient
+        .get<FetchResponse<T>>(endpoint, config)
+        .then((res) => {
+          setData(res.data.results);
           setLoading(false);
-        }
-      });
-    return () => controller.abort();
-  }, []);
+        })
+        .catch((err) => {
+          if (err instanceof CanceledError) {
+            return;
+          } else {
+            setError(err.message);
+            setLoading(false);
+          }
+        });
+      return () => controller.abort();
+    },
+    deps ? [...deps] : []
+  );
 
   return { data, error, isLoading };
 };
